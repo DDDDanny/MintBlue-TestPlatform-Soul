b9f603e45269167b805294c3404477fccd285e66
DDDDanny <707956456@qq.com>
2020-12-03 18:03:12 +0800

    feat: add user api

---
+++ app/Router/UserRouter.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/03 17:57:39
+# @Author  : DannyDong
+# @File    : UserRouter.py
+# @Describe: 用户管理相关路由管理
+
+from flask import Blueprint, request, abort
+
+from app.Common.Result import Result
+from app.Model.UserModel import UserModel
+
+
+# 声明蓝图
+UserBlue = Blueprint('UserBlue', __name__)
+
+
+@UserBlue.route('/user/register', methods=['GET'])
+def user_register():
+    """
+    Desc: 用户注册
+    """
+    return Result().success()
+
+
+@UserBlue.route('/user/login', methods=['POST']):
+def user_login():
+    """
+    Desc: 用户登录
+    """
+    return Result().success()
