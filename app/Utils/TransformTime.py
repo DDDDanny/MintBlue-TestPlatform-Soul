aa6d4abd00e47c52e75c9f1a8489faff1300d2f5
DDDDanny <707956456@qq.com>
2021-02-02 15:35:41 +0800

    feat: add time stamp transform

--- app/Utils/TransformTime.py
+++ app/Utils/TransformTime.py
@@ -6,6 +6,19 @@
 
 import time
 
-# datetime to YYYY-MM-DD HH:mm:SS
 def transform_time(datetime_Obj):
+    """
+    datetime to YYYY-MM-DD HH:mm:SS
+    """    
     return time.strftime("%Y-%m-%d %H:%M:%S", datetime_Obj.timetuple())
+
+
+def time_stamp_transform(datetime_obj):
+    """
+    时间戳转时间
+    """
+    # 转化成时间结构体，并且转化成秒
+    time_struct = time.localtime(datetime_obj // 1000)
+    # 格式化输出
+    new_time = time.strftime("%Y-%m-%d %H:%M:%S", time_struct)
+    return new_time
