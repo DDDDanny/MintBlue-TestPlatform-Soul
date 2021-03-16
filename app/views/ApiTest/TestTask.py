013b9405ce9bb48edbb3fea40533d23f1e7b632d
DDDDanny <707956456@qq.com>
2021-03-16 16:48:29 +0800

    perf: update query data func

--- app/views/ApiTest/TestTask.py
+++ app/views/ApiTest/TestTask.py
@@ -11,7 +11,10 @@ from flask import make_response
 
 from factory import db
 from app.Common.Result import Result
-from app.Model.TaskModel import TaskModel
+from app.Model.UserModel import UserModel
+from app.Model.VersionModel import VersionModel as VM
+from app.Model.TaskModel import TaskModel as TM
+from app.Model.SuiteModel import SuiteModel as SM
 from app.Utils.TransformTime import transform_time, time_stamp_transform
  
  
@@ -40,13 +43,19 @@ class TestTask(object):
 
     # 测试任务列表
     def get_task_list(self, pro_id):
-        sql = ''' select task_id, task_name, task.create_time, task_start_time,
-                task_end_time, task_status, username, version, suite_name from
-                task left join user on creator = user_id left join version on 
-                task.ver_id = version.ver_id left join suite on 
-                task.suite_id = suite.suite_id where task.pro_id="{}"
-                order by task.create_time desc; '''.format(pro_id)
-        data_obj = db.session.execute(sql)
+        # 获取数据对象
+        task_obj = db.session.query(
+            TM.task_id, TM.task_name, TM.create_time, TM.task_start_time, TM.task_end_time, TM.task_status,
+            UserModel.username, VM.version, SM.suite_name
+        ).join(
+            UserModel, UserModel.user_id == TM.creator
+        ).join(
+            VM, VM.ver_id == TM.ver_id
+        ).join(
+            SM, SM.suite_id == TM.suite_id
+        )
+        # 数据对象进行筛选和排序
+        data_obj = task_obj.filter(TM.pro_id == pro_id).order_by(TM.create_time.desc())
         data = [self.__task_info_serializer(item) for item in data_obj]
         res = Result(data).success()
         return make_response(res)
@@ -57,7 +66,7 @@ class TestTask(object):
             res = Result(msg='任务名称不能为空').success()
         else:
             task_id = self.__create_uuid()
-            task_info = TaskModel(
+            task_info = TM(
                 task_id=task_id,
                 task_name=task_name,
                 suite_id=suite_id,
@@ -77,7 +86,7 @@ class TestTask(object):
 
     # 删除任务信息
     def del_task_info(self, task_id):
-        task_info = TaskModel.query.filter_by(task_id=task_id).first()
+        task_info = TM.query.filter_by(task_id=task_id).first()
         if task_info is None:
             res = Result(msg='taskID无效，没有查找到对应的任务信息').success()
         else:
